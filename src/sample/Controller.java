2b29398bf0b665f533e99b564d09f8617e8dba99
Alexandr <tank006@yandex.ru>
2021-04-08 19:41:39 +0400

    continue project

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,10 @@
 package sample;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -11,12 +13,20 @@ import java.net.Socket;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class Controller implements Initializable {
+public class Controller {
     @FXML
     TextArea textArea;
-
     @FXML
     TextField textField;
+    @FXML
+    HBox bottomPanel;
+    @FXML
+    HBox upperPanel;
+    @FXML
+    TextField loginField;
+    @FXML
+    PasswordField passwordField;
+
 
     Socket socket;
     DataInputStream in;
@@ -25,28 +35,48 @@ public class Controller implements Initializable {
     public static final String ADDRESS = "localhost";
     public static final int PORT = 6001;
 
+    private boolean isAuthorized;
 
-    @FXML
-    void sendMsg() {
-        try {
-            out.writeUTF(textField.getText());
-            textField.clear();
-            textField.requestFocus();
-        } catch (IOException e) {
-            e.printStackTrace();
+    public void setAuthorized(boolean authorized){
+        this.isAuthorized = authorized;
+
+        if (!isAuthorized){
+            upperPanel.setVisible(true);
+            upperPanel.setManaged(true);
+
+            bottomPanel.setVisible(false);
+            bottomPanel.setManaged(false);
+        } else {
+            upperPanel.setVisible(false);
+            upperPanel.setManaged(false);
+
+            bottomPanel.setVisible(true);
+            bottomPanel.setManaged(true);
         }
     }
 
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    void sendMsg() {
+
+    }
+
+    public void connect() {
         try {
             socket = new Socket(ADDRESS, PORT);
 
-            in = new DataInputStream(socket.getInputStream());
-            out = new DataOutputStream(socket.getOutputStream());
-
             new Thread(() -> {
                 try {
+                    while (true) {
+                        String str  = in.readUTF();
+                        if ("/auth-OK".equals(str)) {
+                            setAuthorized(true);
+                            textArea.clear();
+                            break;
+                        } else {
+                            textArea.appendText(str + "\n");
+                        }
+                    }
+
                     while (true) {
                         String str  = in.readUTF();
                         if ("/serverClosed".equals(str)) {
@@ -62,10 +92,43 @@ public class Controller implements Initializable {
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
+                    setAuthorized(false);
                 }
             }).start();
         } catch (IOException e) {
             e.printStackTrace();
+            textArea.appendText("Connection refused \n");
+        }
+    }
+
+    public void tryToAuth(ActionEvent actionEvent) {
+        if (socket == null || socket.isClosed()){
+            connect();
+        }
+        try {
+            out.writeUTF("/auth " + loginField.getText() + " " + passwordField.getText());
+            loginField.clear();
+            passwordField.clear();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void disconnect(){
+        if (socket != null){
+            if (socket.isClosed()){
+                try {
+                    out.writeUTF("/end");
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+
+                try {
+                    socket.close();
+                } catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/server/ConsoleServer.java
+++ src/server/ConsoleServer.java
@@ -18,31 +18,39 @@ public class ConsoleServer {
         Socket socket = null; // удаленная (remote) сторона
 
         try {
+            AuthService.connect();
             server = new ServerSocket(6001);
             System.out.println("Server started");
 
             while (true) {
                 socket = server.accept();
                 System.out.printf("Client [%s] connected\n", socket.getInetAddress());
-
-                users.add(new ClientHandler(this, socket));
+                new ClientHandler(this, socket);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             try {
-                server.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-            try {
                 socket.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            try {
+                server.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            AuthService.disconnect();
         }
+    }
+
+    public void subscribe (ClientHandler client){
+        users.add(client);
+    }
+
+    public void unSubscribe (ClientHandler client){
+        users.remove(client);
     }
 
     public void broadcastMessage(String str) {
